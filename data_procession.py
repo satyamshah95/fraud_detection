986e13fc8ee923b9b0bfad67bdd33b1be1f456b4
satyamshah95 <satyamshah1995@gmail.com>
2023-10-07 17:49:56 +0530

    added ml model code

--- data_procession.py
+++ data_procession.py
@@ -1,1 +1,10 @@
 print ("adding all the data for fraud detection")
+
+a= 5
+b= 6
+
+def sum(a,b):
+    c=a+b
+    return a
+
+print ("not correct")
---
+++ ml_model.py
@@ -0,0 +1,1 @@
+print ("adding all related ML code")
